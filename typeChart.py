6d837fcdff1dc5233faf8ff93480ed7d3d93e02f
Not Sus Impostor <111621644+a-sussy-impostor@users.noreply.github.com>
2024-05-03 21:31:37 +0800

    Update typeChart.py

--- typeChart.py
+++ typeChart.py
@@ -1,3 +1,12 @@
-def match():
+class Type():
+  def __init__(self, weak, resist, immune):
+    self.weak = weak
+    self.resist = resist
+    self.immune = immune
+    del weak, resist, immune
+
+# create class types later
+
+def match(type1,type2):
   # dummy
   return 1
