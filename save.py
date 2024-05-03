996df26b6c6759fa84048efe06bebc112de64afc
Not Sus Impostor <111621644+a-sussy-impostor@users.noreply.github.com>
2024-05-03 23:09:03 +0800

    Create save.py

---
+++ save.py
@@ -0,0 +1,18 @@
+import pickle
+
+def save(key,data):
+  with open("savedata.pk","rb") as f:
+    g = pickle.load(f)
+  g[key] = data
+  with open("savedata.pk","wb") as f:
+    pickle.dump(g)
+  # del f,g
+
+def load(key):
+  with open("savedata.pk","rb") as f:
+    pickle.load(f)
+  # del f
+  try:
+    return pickle[key]
+  except:
+    return
