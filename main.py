a98d44989efc4a7c50b7c739809faad498a636ce
Not Sus Impostor <111621644+a-sussy-impostor@users.noreply.github.com>
2024-05-03 21:19:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from os import system as sys
 sys("pip3 install colorama")
 
 from base import *
-from colorama import Fore, Style as fore, style
+from colorama import Fore as fore
 testing = Disk("Pikachu")
 
 print(testing.name)
