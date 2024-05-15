07778b21c3c2728b63dba9cd37846a78de4a217b
Not Sus Impostor <111621644+a-sussy-impostor@users.noreply.github.com>
2024-05-15 11:23:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from os import system as sys
-sys("pip3 install colorama")
+from prerun import *
 
 from base import *
-from colorama import Fore as fore
+
 testing = Disk("Pikachu","Electric",25,[35,55,40,50,50,90],["Thunderbolt","Electric",90,"S",None,None],2,1500)
 testing2 = Disk("Charmander","Fire",25,[35,55,40,50,50,90],["Flamethrower","Fire",90,"S",None,None],2,1500)
 
 print(testing.name)
-print("Hello World")
+print(sBright + "Hello World")
 print(testing.Attack(testing2))
+print(cRed + "hi")
+print(cCyan + "hello")
