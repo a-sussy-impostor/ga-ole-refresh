a1dbb6290bf7f86dbb1463e9e822a1d5a4b7e517
Not Sus Impostor <111621644+a-sussy-impostor@users.noreply.github.com>
2024-05-19 10:18:27 +0800

    Added logging feature

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from prerun import *
-sys("pip3 install pickle")
+from os import getenv
+
+if os.getenv("GITHUB_ACTIONS") == "true":
+  print("Running on github actions")
+  sys("pip3 install pickle")
+  sys("pip install logging")
+
+import logging
 from base import *
+
+logging.basicConfig(level=logging.INFO, filename="latest.log", filemode="w", format="%(asctime)s [%(levelname)s]: %(message)s")
 
 testing = Disk("Pikachu","Electric",25,[35,55,40,50,50,90],["Thunderbolt","Electric",90,"S",None,None],2,1500)
 testing2 = Disk("Charmander","Fire",25,[35,55,40,50,50,90],["Flamethrower","Fire",90,"S",None,None],2,1500)
 
-print(testing.name)
-print(sBright + "Hello World")
-print(testing.Attack(testing2))
-print(cRed + "hi")
-print(cCyan + "hello")
+logging.info(testing.name)
+logging.info(sBright + "Hello World")
+logging.info(testing.Attack(testing2))
+logging.warning(cRed + "hi")
+logging.warning(cCyan + "hello")
